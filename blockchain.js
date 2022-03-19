f060012d7c1960d2f8d56f6274602b80feff7639
priyesh20 <priyeshagrawal20@gmail.com>
2022-03-19 19:54:09 +0530

    blockchain validation

--- blockchain.js
+++ blockchain.js
@@ -27,18 +27,41 @@ class BlockChain {
         return new Block(0, Date.now(), {}, "")
     }
 
+    // Function to get the last block hash
     getLastBlockHash() {
         return this.chain[this.chain.length - 1].hash;
     }
 
+    // Dunction to add the block
     addBlock(block) {
         block.hashOfLastBlock = this.getLastBlockHash();
         block.hash = block.createHash();
         this.chain.push(block)
     }
+
+    // Function to validate the blockchain
+    isValidBlockchain() {
+        for (let i=1; i < this.chain.length; i++) {
+            const block = this.chain[i];
+            const newHash =  block.createHash();
+            // condition based on creating temporary hash from data to compare the block.hash value
+            if (newHash !== block.hash) return false;
+
+            // condition to check the hash of previous node and hashOfLastBlock value of current block
+            if ( this.chain[i - 1].hash !== block.hashOfLastBlock ) return false;
+        }
+        return true;
+    }
 }
 
 let handsOnCoin = new BlockChain();
 handsOnCoin.addBlock(new Block(1, Date.now(), { balance: 100 }));
+handsOnCoin.addBlock(new Block(2, Date.now(), { balance: 200 }));
+handsOnCoin.addBlock(new Block(3, Date.now(), { balance: 300 }));
+
+/** comment out the below to lines to change value of block 2 in the change to check validation
+handsOnCoin.chain[2].data.balance = 400
+handsOnCoin.chain[2].hash = handsOnCoin.chain[2].createHash() **/
 
 console.log("handsOnCoin: ", handsOnCoin);
+console.log("validate: ", handsOnCoin.isValidBlockchain());
